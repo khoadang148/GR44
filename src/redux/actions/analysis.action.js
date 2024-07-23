56598201167e537e3bbbab160de6b982a24fcc73
khoadang148 <khoapdse173532@fpt.edu.vn>
2024-07-23 17:32:58 +0700

    chart

--- src/redux/actions/analysis.action.js
+++ src/redux/actions/analysis.action.js
@@ -1,23 +1,27 @@
+// analysisActions.js
 import axios from 'axios';
+import {FETCH_ANALYSIS_REQUEST,
+    FETCH_ANALYSIS_SUCCESS,
+    FETCH_ANALYSIS_FAILURE, } from '../actionType';
+  
 
-// Action Types
-export const FETCH_ANALYSIS_DATA_REQUEST = 'FETCH_ANALYSIS_DATA_REQUEST';
-export const FETCH_ANALYSIS_DATA_SUCCESS = 'FETCH_ANALYSIS_DATA_SUCCESS';
-export const FETCH_ANALYSIS_DATA_FAILURE = 'FETCH_ANALYSIS_DATA_FAILURE';
+const API_URL = 'https://669f7703b132e2c136fdf01d.mockapi.io/analysis';
 
-// Action Creators
-export const fetchAnalysisData = () => async (dispatch) => {
-  dispatch({ type: FETCH_ANALYSIS_DATA_REQUEST });
-  try {
-    const response = await axios.get('https://667e5671297972455f67ee82.mockapi.io/projectojt/api/v1/users');
-    dispatch({
-      type: FETCH_ANALYSIS_DATA_SUCCESS,
-      payload: response.data,
-    });
-  } catch (error) {
-    dispatch({
-      type: FETCH_ANALYSIS_DATA_FAILURE,
-      payload: error.message,
-    });
-  }
+export const fetchAnalysisData = () => {
+  return async (dispatch) => {
+    dispatch({ type: FETCH_ANALYSIS_REQUEST });
+
+    try {
+      const response = await axios.get(API_URL);
+      dispatch({
+        type: FETCH_ANALYSIS_SUCCESS,
+        payload: response.data[0], 
+      });
+    } catch (error) {
+      dispatch({
+        type: FETCH_ANALYSIS_FAILURE,
+        error: error.message,
+      });
+    }
+  };
 };
